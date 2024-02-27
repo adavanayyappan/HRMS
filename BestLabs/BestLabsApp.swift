da16add5020f8491021122c7fe1dbbcb7308a489
suresh-iosdev <suresh@jankosoft.com>
2024-02-27 14:08:28 +0530

    Initial Commit

---
+++ BestLabs/BestLabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestLabsApp.swift
+//  BestLabs
+//
+//  Created by Suresh Swaminathan on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct BestLabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
